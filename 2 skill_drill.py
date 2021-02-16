44d011ccd522ba2ab94866349df3372fbbab6c35
DONALDRMILAM <77140350+DONALDRMILAM@users.noreply.github.com>
2021-02-16 15:22:50 -0800

    Add files via upload

---
+++ 2 skill_drill.py
@@ -0,0 +1,8 @@
+x= int(input ("Enter the first number: "))
+y= int(input ("Enter the second number: "))
+z= int(input ("Enter the third number: "))
+w= int(input ("Enter the third number: "))
+total=str(w+y*z-w)
+
+print("The total of those three numbers is"+ total)
+(the total for those three numbers is 44)
---
+++ 5skill_drill.py
@@ -0,0 +1,9 @@
+x= int(input ("Enter the first number: "))
+y= int(input ("Enter the second number: "))
+z= int(input ("Enter the third number: "))
+
+total=str(x**y%z)
+
+print("The total of those three numbers is"+ total)
+
+The total of those three numbers is2
