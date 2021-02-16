0f7e60f01851d115c9312b936f1a4091c192e9f4
DONALDRMILAM <77140350+DONALDRMILAM@users.noreply.github.com>
2021-02-16 15:23:58 -0800

    Add files via upload

---
+++ math drills/3skill_drill.py
@@ -0,0 +1,7 @@
+x= int(input ("Enter the first number: "))
+y= int(input ("Enter the second number: "))
+z= int(input ("Enter the third number: "))
+
+total=str(x//y-z)
+
+print("The total of those three numbers is"+ total)
---
+++ math drills/4skill_drill.py
@@ -0,0 +1,9 @@
+x= int(input ("Enter the first number: "))
+y= int(input ("Enter the second number: "))
+z= int(input ("Enter the third number: "))
+w= int(input ("Enter the third number: "))
+q= int(input ("Enter the third number: "))
+total=str(w-y/z+w-q)
+
+print("The total of those three numbers is"+ total)
+The total of those three numbers is11.5
