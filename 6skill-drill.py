90fb04de76fab7d6c97fccf8e26d04f14f6f89a6
DONALDRMILAM <77140350+DONALDRMILAM@users.noreply.github.com>
2021-02-16 15:25:28 -0800

    Delete 6skill-drill.py

--- 6skill-drill.py
+++
@@ -1,9 +0,0 @@
-x= int(input ("Enter the first number: "))
-y= int(input ("Enter the second number: "))
-z= int(input ("Enter the third number: "))
-w= int(input ("Enter the third number: "))
-q= int(input ("Enter the third number: "))
-total=str(w+(y*z/w-q))
-
-print("The total of those three numbers is"+ total)
-
